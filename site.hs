e77864e703e7c4b4a1e19dcd8724aaf2156f175a
Felix Schnizlein <felix@fix3d.net>
2014-08-06 16:48:47 +0200

    Started integrating gallery functionality

--- site.hs
+++ site.hs
@@ -1,17 +1,24 @@
 {-# LANGUAGE OverloadedStrings  #-}
 
+import Control.Monad
 import Control.Applicative
 
 import System.Locale (defaultTimeLocale)
 
+
+
+import qualified Data.Map as M
 import Data.Monoid
 import Data.Time.Clock (getCurrentTime)
 import Data.Time.Format (formatTime)
 import Data.List (isPrefixOf, intercalate)
 import qualified Data.Map as M
+import qualified Data.Set as S
 
 import qualified Text.Pandoc.Options as O
 import Text.Highlighting.Kate.Styles (pygments)
+
+import Debug.Trace
 
 import Hakyll
 
@@ -19,22 +26,85 @@ siteRoot = "http://none.io"
 siteKeywords = "Programming, Haskell, C++, Felix, Schnizlein, Personal, Blog, Vegan, Food"
 siteDescription = "Personal blog of Felix. Writing what ever comes to mind. Mainly about Haskell and other programming languages. But also about music, politics and cooking vegan food."
 
+data Galleries = Galleries
+  { gaMap :: M.Map String [Identifier]
+  , gaMakeId :: String -> Identifier
+  , gaPattern :: Pattern
+  , gaDependency :: Dependency
+  }
+
+
+instance Show Galleries where
+    show x = show $ gaMap x
+
+
+getImages :: String -> Galleries -> Maybe [Identifier]
+getImages g = M.lookup g . gaMap
+
+
+
+
+buildGalleries :: MonadMetadata m => String -> (String -> Identifier) -> m Galleries
+buildGalleries pattern makeId = makeGal =<< getMatches glob
+        where
+            glob        = fromGlob pattern
+            parseAll    = map (parseIdentifier pattern)
+            makeMap     = M.fromListWith (++)
+            makeGal mat = return $ Galleries (makeMap $ parseAll mat) makeId glob (PatternDependency glob mat)
+
+parseIdentifier :: String -> Identifier -> (String, [Identifier])
+parseIdentifier p s = (name name', [s])
+    where
+        name [] = "default"
+        name x  = init x
+        name'   = removeFileName $ removePath $ toFilePath s
+
+        path    = takeWhile (/= '*') p
+        removePath = drop (length path)
+        removeFileName   = reverse . dropWhile (/= '/') . reverse
+
+
+galleriesRules :: Galleries -> (String -> [Identifier] -> Pattern -> Rules ()) -> Rules ()
+galleriesRules gal rule = imageDeps >> (forM_ (M.assocs $ gaMap gal) $ \(name, identifiers) ->
+    rulesExtraDependencies [gaDependency gal] $
+        create [gaMakeId gal name] $ rule name identifiers (fromList identifiers))
+    where
+      imageDeps = rulesExtraDependencies [gaDependency gal] $ 
+        match (gaPattern gal) $ do
+            route idRoute
+            compile copyFileCompiler
+
+imagesField :: String -> String -> [Identifier] -> Context a
+imagesField name gal idfs = listField name (constField "gallery" gal <> field "image" (return . itemBody)) (mapM (makeItem . toUrl . toFilePath) idfs)
+
+
+galleryContext :: Galleries -> (String -> Snapshot) -> Context a
+galleryContext gal mkSnapshot = mconcat $ forM (M.assocs $ gaMap gal) $ \(name, _) ->
+    field name (\_ -> itemBody <$> loadSnapshot (gaMakeId gal name) (mkSnapshot name))
+
 main :: IO ()
 main 
     = hakyllWith config $ do
 
     tags <- buildTags "posts/*.md" $ fromCapture "tags/*.html"
+    galleries <- buildGalleries "gal/**" $ fromCapture "gallery/*.html"
+    _ <- preprocess (print galleries)
 
-    tagsRules tags $ compileTags tags
+    galleriesRules galleries $ \name images pattern -> do
+
+        let ctx = imagesField "images" name images <> defaultContext <> constField "gal" name
+        route idRoute
+        compile $ makeItem name
+            >>= loadAndApplyTemplate "templates/gallery.html" ctx
+            >>= saveSnapshot ("gallery_" ++ name)
+            >>= loadAndApplyTemplate "templates/base.html" defaultContext
+            >>= relativizeUrls
 
     -- read templates
     match "templates/*" $ compile templateCompiler
 
     -- copy static images
-    match (  "static/*.png" 
-        .||. "static/*.svg"
-        .||. "static/*.gif"
-        .||. "g/*/*"
+    match (  "static/**"
         .||. "robots.txt") $ do
         route idRoute
         compile copyFileCompiler
@@ -107,32 +177,12 @@ main
     where
         staticPages = ["notice.md", "about.md"]
 
-absoluteUrls :: String -> Item String -> Compiler (Item String)
-absoluteUrls
-    root = return . fmap (relativizeUrlsWith root)
-
-compileTags :: Tags -> String -> Pattern -> Rules ()
-compileTags
-    tags tag _ = do
-        route idRoute
-        compile $ makeItem ""
-            >>= loadAndApplyTemplate "templates/category.html" tagsCtx
-            >>= loadAndApplyTemplate "templates/base.html" tagsCtx
-            >>= relativizeUrls
-        where
-            tagsCtx = constField "title" ("Tagged with " ++ tag) 
-                   <> listField "posts" (postCtx tags) (take 10 <$> (recentFirst =<< loadAll "posts/*.md"))
-                   <> defaultContext
 
 sitemapCtx :: Tags -> Context String
 sitemapCtx
     tags = defaultContext
     <> listField "posts" (postCtx tags) (recentFirst =<< loadAll "posts/*.md")
     <> nowField "created" "%Y-%m-%d"
-
-nowField :: String -> String -> Context String
-nowField
-    key fmt = field key $ \_ -> unsafeCompiler $ (formatTime defaultTimeLocale fmt <$> getCurrentTime)
 
 
 indexCtx :: Tags -> Context String
@@ -153,14 +203,14 @@ postCtx
         <> dateField "date" "%B %d, %Y"
         <> dateField "created" "%Y-%m-%d"
         <> modificationTimeField "mod" "%Y-%m-%d"
-        <> field "math" mathjax
-        
+       
 
 config :: Configuration
 config 
     = defaultConfiguration
         { deployCommand = "rsync -avz -e ssh ./_site/ none.io:/home/felixsch/html/"
         }
+
 
 feedConfig :: FeedConfiguration
 feedConfig
@@ -172,18 +222,20 @@ feedConfig
     , feedRoot          = "http://none.io"
     }
 
-mathjax :: Item String -> Compiler String
-mathjax
-    item = do
-        metadata <- getMetadata (itemIdentifier item)
-        return $ case M.lookup "math" metadata of
-            Just "true" -> script
-            otherwise -> ""
-    where
-      script = "<script type=\"text/javascript\" src=\"http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML\" />"
-       
+
+nowField :: String -> String -> Context String
+nowField
+    key fmt = field key $ \_ -> unsafeCompiler $ (formatTime defaultTimeLocale fmt <$> getCurrentTime)
+
+
 tagList :: Tags -> String
 tagList tags = intercalate "," $ map fst $ tagsMap tags
+
+
+absoluteUrls :: String -> Item String -> Compiler (Item String)
+absoluteUrls
+    root = return . fmap (relativizeUrlsWith root)
+
 
 pandocOptions :: O.WriterOptions
 pandocOptions = defaultHakyllWriterOptions
